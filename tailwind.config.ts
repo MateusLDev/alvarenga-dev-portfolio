92c26a55647b238ec1a39487dab23fe3b8d4605a
Mateus Lopes <mateus.lopes_stormgroup@prestador.globo>
2024-11-12 23:56:47 -0300

    chore: Setting tailwind colors variables & front family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,24 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['DMSans', 'sans-serif']
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: 'rgb(var(--primary))',
+
+        'background-primary': 'rgb(var(--bg-primary))',
+        'background-secondary': 'rgb(var(--bg-secondary))',
+        'background-tertiary': 'rgb(var(--bg-tertiary))',
+        'background-quaternary': 'rgb(var(--bg-quaternary))',
+
+        'white-500': 'rgb(var(--white-500))',
+        'white-400': 'rgb(var(--white-400))',
+        'white-300': 'rgb(var(--white-300))',
+        'white-200': 'rgb(var(--white-200))',
+        'white-100': 'rgb(var(--white-100))',
+
+        'gray-500': 'rgb(var(--gray-500))',
       },
     },
   },
