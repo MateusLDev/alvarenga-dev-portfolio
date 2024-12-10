e91c2a7fd0dd9677c4b05af012eb36c8b9362210
Mateus Lopes <mateus.lopes_stormgroup@prestador.globo>
2024-12-10 19:11:02 -0300

    feature: Downloading CV and go to mentoring

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image'
 import Button from '../button'
 import { useTranslations } from 'next-intl'
@@ -5,8 +6,16 @@ import { openLink } from '@/utils/utils'
 
 const Hero: React.FC = () => {
   const t = useTranslations('hero')
+
+  const downloadCV = () => {
+    const link = document.createElement("a");
+    link.href = "/cv.pdf";
+    link.download = "Lucas Alvarenga CV.pdf";
+    link.click();
+  };
+
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-center md:justify-around gap-10 lg:gap-20 mt-40 mb-20">
+    <div className="flex flex-col lg:flex-row items-center justify-center md:justify-around gap-10 lg:gap-20 mt-40 mb-20 bg-[url('/images/hero-bg-png')]">
       <Image
         src="/images/hero-image-mobile.png"
         alt="Imagem do Lucas Alvarenga"
@@ -22,7 +31,7 @@ const Hero: React.FC = () => {
 
         <p className="max-w-2xl font-medium text-base lg:text-lg text-center lg:text-left text-white-400 mt-5">
           {t('description')}
-          <span className="text-primary cursor-pointer"> @alvarenga.dev</span>
+          <span onClick={() => openLink('https://instagram.com/alvarenga.dev')} className="text-primary cursor-pointer"> @alvarenga.dev</span>
         </p>
 
         <div className="flex items-center justify-center lg:justify-normal gap-6 mt-7">
@@ -31,7 +40,7 @@ const Hero: React.FC = () => {
             color="primary"
             text={t('mentoringButton')}
           />
-          <Button color="secondary" text={t('cvButton')} />
+          <Button onClick={downloadCV} color="secondary" text={t('cvButton')} />
         </div>
       </div>
 
